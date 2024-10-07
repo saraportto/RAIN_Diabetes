cc396a29c0dea178efa9b1d78b17ffef6948f9ed
Valeria <vlagosmolins@gmail.com>
2024-10-07 13:14:38 +0200

    diabetes_RB_trabajo

--- diabetes_diagnose/example.py
+++ diabetes_diagnose/example.py
@@ -6,10 +6,10 @@ np.random.seed(42)  # Para que los resultados sean reproducibles
 
 # Simulamos variables: edad, nivel de glucosa, IMC y diabetes (sí/no)
 data = pd.DataFrame({
-    'Edad': np.random.choice([0, 1, 2], size=1000),  # 0: joven, 1: adulto, 2: mayor
-    'NivelGlucosa': np.random.choice([0, 1], size=1000),  # 0: bajo, 1: alto
-    'IMC': np.random.choice([0, 1, 2], size=1000),  # 0: bajo, 1: normal, 2: alto
-    'Diabetes': np.random.choice([0, 1], size=1000)  # 0: no, 1: sí
+    'Edad': np.random.choice([0, 1, 2], size=10000000),  # 0: joven, 1: adulto, 2: mayor
+    'NivelGlucosa': np.random.choice([0, 1], size=10000000),  # 0: bajo, 1: alto
+    'IMC': np.random.choice([0, 1, 2], size=10000000),  # 0: bajo, 1: normal, 2: alto
+    'Diabetes': np.random.choice([0, 1], size=10000000)  # 0: no, 1: sí
 })
 
 # Guardamos el dataset simulado en un archivo CSV
